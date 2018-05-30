b0e49b04e8e4d58b3a948c049cfb334711489d27
Crystal2333 <32433913+Crystal2333@users.noreply.github.com>
2018-05-30 22:24:55 +0800

    Add files via upload

---
+++ pages/map/map.js
@@ -0,0 +1,15 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    markers: [{
+      latitude: 31.025670,
+      longitude: 121.43645,
+      iconPath: './定位.png',
+      width: 30,
+      height: 30,
+    }]
+  }
+})
